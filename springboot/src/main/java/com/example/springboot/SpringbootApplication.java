09fb3f5c7e8fe7b52116f83920cf71361380fac5
tauheed <tauheed1patel@gmail.com>
2023-11-07 15:01:57 +0530

    Adding Files To Github

---
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// this is the main start point of spring as java is based on oops
+// that's why we have a main method here
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
