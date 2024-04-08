066536a963c2b74140fb0fb9c741507bf17d5f57
Mohammad Tauheed Raza Patel <80320712+mohammadTauheedrazapatel30@users.noreply.github.com>
2024-04-08 13:20:31 +0530

    Updated With the Latest code

--- StudentController.java
+++ StudentController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.entity.StudentEntity;
@@ -62,5 +67,23 @@ public class StudentController {
 	    {
 	        return studentService.findById(id);
 	    }
-
+	
+	// localhost:8080/delete/{id}
+//	@RequestMapping(method=RequestMethod.DELETE, value="delete/{id}")
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<String> deleteStudent(@PathVariable(value = "id") long id){
+		// Optional is used when we want to run the method on null values !
+		Optional<StudentEntity> existingStudent = studentService.findById(id);
+		if(existingStudent.isPresent()) {
+		   this.studentService.deleteStudent(existingStudent.get());
+		   return ResponseEntity.ok("Deleted Successfully !");
+		}
+		return ResponseEntity.notFound().build();
+	}
+ 
+//	@PutMapping("update/{id}")
+//	public ResponseEntity<StudentEntity> updateStudent(@PathVariable(value="id") long id, @RequestBody StudentEntity students){
+//        StudentEntity updateStudent = studentService.updateStudent(id,students);
+//		  return ResponseEntity.ok(updateStudent);
+//	}
 }
